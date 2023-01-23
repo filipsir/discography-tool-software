514ffc851cd05a4d2166fa58e05aed72ff480b8f
roberthonc <51189589+roberthonc@users.noreply.github.com>
2023-01-23 13:29:48 +0100

    Create single-nahravky.php

---
+++ templates/content/single-nahravky.php
@@ -0,0 +1,17 @@
+<section id="section-page-intro">
+
+    <?php get_template_part( 'templates/sections/global', 'page-intro' ); ?>
+
+</section>
+
+<section id="section-record-bar">
+
+    <?php get_template_part( 'templates/sections/record', 'bar' ); ?>
+
+</section>
+
+<section id="section-record-content">
+
+    <?php get_template_part( 'templates/sections/record', 'content' ); ?>
+
+</section>
